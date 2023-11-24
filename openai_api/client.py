e3341e18f31d003cadfb996de15a169acaadf54b
Python Code <77999081+pycodebr@users.noreply.github.com>
2023-11-24 16:04:49 -0300

    Update client.py

--- openai_api/client.py
+++ openai_api/client.py
@@ -7,7 +7,7 @@ def get_car_ai_bio(model, brand, year):
     Me mostre uma descrição de venda para o carro {} {} {} em apenas 250 caracteres. Fale coisas específicas desse modelo.
     Descreva especificações técnicas desse modelo de carro.
     '''
-    openai.api_key = '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'
+    openai.api_key = 'API_KEY'
     prompt = prompt.format(brand, model, year)
     response = openai.Completion.create(
         model='text-davinci-003',
