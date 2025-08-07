bfe303793d284cc977275b3467a31cc820227b24
pycodebr <pycodebr@gmail.com>
2025-08-07 18:15:38 -0300

    update dependencies

--- app/settings.py
+++ app/settings.py
@@ -84,6 +84,10 @@ DATABASES = {
         'PASSWORD': 'djangomaster',
         'HOST': 'localhost',
         'PORT': '5432',
+    },
+    'dev': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
